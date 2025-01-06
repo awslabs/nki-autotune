a6b88ccc4e77a5264701c48bb2b97f1175582b6a
weiT1993 <weittang@amazon.com>
2025-01-06 17:36:38 +0000

    fix NKI kernel return

--- test/rmsnorm.py
+++ test/rmsnorm.py
@@ -12,33 +12,26 @@ from src.kernels import stack_allocated_fused_rms_norm_qkv, allocated_fused_rms_
 
 
 def get_autotune_configs():
-    """
-    Define a list of configuration dictionaries representing the specific design choices for autotuning.
-
-    Returns:
-        list: A list of dictionaries, each containing configuration parameters for TILES_IN_BLOCK_M,
-                TILES_IN_BLOCK_N, and TILES_IN_BLOCK_K.
-    """
-    TILES_IN_BLOCK_M_options = [4]
-    TILES_IN_BLOCK_N_options = [2, 4]
-    TILES_IN_BLOCK_K_options = [2, 4]
-    params = list(
-        product(
-            TILES_IN_BLOCK_M_options,
-            TILES_IN_BLOCK_N_options,
-            TILES_IN_BLOCK_K_options,
-        )
-    )
-    configs = []
-    for TILES_IN_BLOCK_M, TILES_IN_BLOCK_N, TILES_IN_BLOCK_K in params:
-        config = {
-            "TILES_IN_BLOCK_M": TILES_IN_BLOCK_M,
-            "TILES_IN_BLOCK_N": TILES_IN_BLOCK_N,
-            "TILES_IN_BLOCK_K": TILES_IN_BLOCK_K,
-        }
-        configs.append(config)
-    random.shuffle(configs)
+    configs = [
+        {"multi_buffer": 1},
+        {"multi_buffer": 2},
+    ]
     return configs
+
+
+def rmsnorm_golden(hidden: np.ndarray, weights: np.ndarray):
+    """Fused RMS Norm and Matmul
+
+    Args:
+        hidden (np.ndarray): input tensor
+        weights (np.ndarray): matmul weights
+    """
+    s_hidden = hidden * hidden
+    ms_hidden = np.mean(s_hidden, axis=-1, keepdims=True)
+    rms_hidden = np.sqrt(ms_hidden)
+    rms_norm_hidden = hidden / rms_hidden
+    output_np = np.matmul(rms_norm_hidden, weights)
+    return output_np
 
 
 if __name__ == "__main__":
@@ -47,27 +40,13 @@ if __name__ == "__main__":
     dim = 512
     d_head = 16
 
-    hidden = np.random.rand(batch, seqlen, dim).astype(np.float32)
-    weights = np.random.rand(dim, d_head).astype(np.float32)
-
-    stack_allocated_output_nki = stack_allocated_fused_rms_norm_qkv(hidden, weights)
-    print(
-        f"stack_allocated_output_nki = {stack_allocated_output_nki} {stack_allocated_output_nki.shape}"
+    hidden = nt.tensor[[batch, seqlen, dim], nl.bfloat16]
+    weights = nt.tensor[[dim, d_head], nl.bfloat16]
+    tuner = AutotuneKernel.trace(
+        allocated_fused_rms_norm_qkv,
+        iters=10,
+        configs=get_autotune_configs(),
+        max_workers=1,
+        show_compiler_tb=True,
     )
-
-    allocated_output_nki = allocated_fused_rms_norm_qkv(hidden, weights)
-    print(f"allocated_output_nki = {allocated_output_nki} {allocated_output_nki.shape}")
-
-    # numpy RMSNorm
-    s_hidden = hidden * hidden
-    ms_hidden = np.mean(s_hidden, axis=-1, keepdims=True)
-    rms_hidden = np.sqrt(ms_hidden)
-    rms_norm_hidden = hidden / rms_hidden
-    output_np = np.matmul(rms_norm_hidden, weights)
-    print(f"output_np = {output_np} {output_np.shape}")
-
-    for output_nki in [stack_allocated_output_nki, allocated_output_nki]:
-        allclose = np.allclose(output_np, output_nki, atol=1e-4, rtol=1e-2)
-        if allclose:
-            print("NKI and NumPy match")
-        assert allclose
+    tuner(hidden, weights)
