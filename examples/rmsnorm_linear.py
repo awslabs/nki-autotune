e2c72a8fbc495a3ba6bec851d44fbf5df4a9622d
Wei Tang <weittang@amazon.com>
2025-05-24 02:47:42 +0000

    profiled online rmsnorm

--- examples/rmsnorm_linear.py
+++ examples/rmsnorm_linear.py
@@ -9,6 +9,7 @@ import numpy as np
 from neuronpy.core.language import bfloat16
 
 from autotune.cache.directories import get_cache_dir
+from autotune.cache.visualize import plot_metrics_vs_k_comparison
 from autotune.core.utils import GEMMCompatibility
 from autotune.tune.benchmark import Benchmark
 from autotune.tune.job import ProfileJobs
@@ -81,9 +82,8 @@ if __name__ == "__main__":
     mn_shapes = [2048]
     k_shapes = [1024, 2048, 4096, 8192, 16384]
     MNK = list(product(mn_shapes, mn_shapes, k_shapes))
-    MNK = [(2048, 2048, 4096)]
     for M, N, K in MNK:
-        # profile_baseline(workload_name, M, N, K)
+        profile_baseline(workload_name, M, N, K)
         run_autotune_jobs(workload_name, M, N, K)
-    #     plot_metrics_vs_k_comparison(workload_name)
-    # plot_metrics_vs_k_comparison(workload_name)
+        plot_metrics_vs_k_comparison(workload_name)
+    plot_metrics_vs_k_comparison(workload_name)
