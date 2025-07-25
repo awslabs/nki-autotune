12d2a5fc082eb5b4d7f7c0fb75e32eea4cf57da1
weiT1993 <weittang@amazon.com>
2025-07-25 07:10:37 +0000

    fixed lhsT @ rhs

--- examples/lhsT_rhs.py
+++ examples/lhsT_rhs.py
@@ -1,30 +1,33 @@
 # Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0
 
-from typing import Any, Dict, List
+from itertools import permutations, product
 
 import numpy as np
 from neuronpy.core.language import bfloat16
 
+from autotune.cache.visualize import plot_metric
+from autotune.core.benchmark import Benchmark
 from autotune.core.job import ProfileJobs
-from autotune.modules.matmul import GEMMCorrectness
-from autotune.modules.meta_lhsT_rhs import MetaTGEMM
+from autotune.core.tune import generate_configs
+from autotune.modules.matmul import GEMMCompatibility, GEMMCorrectness
 
 
 def get_configs():
-    configs = [
-        {
-            "NUM_BLOCK_M": 2,
-            "NUM_BLOCK_N": 1,
-            "NUM_BLOCK_K": 4,
-            "loop_order_str": "MNK",
-            "tensor_positions": {"lhsT_block": 0, "rhs_block": 1},
-        }
-    ]
+    size_options = [1, 2, 4, 8, 16]
+    loop_orders = list(permutations("MNK"))
+    loop_orders = ["".join(loop_order) for loop_order in loop_orders]
+    params = {
+        "NUM_BLOCK_M": size_options,
+        "NUM_BLOCK_N": size_options,
+        "NUM_BLOCK_K": size_options,
+        "loop_order": loop_orders,
+    }
+    configs = generate_configs(**params)
     return configs
 
 
-def get_jobs(M: int, N: int, K: int, configs: List[Dict[str, Any]]):
+def add_jobs(jobs: ProfileJobs, M: int, N: int, K: int):
     data_type = "float32"
     if data_type == "float32":
         data_type = np.float32
@@ -36,60 +39,29 @@ def get_jobs(M: int, N: int, K: int, configs: List[Dict[str, Any]]):
         raise NotImplementedError(f"{data_type} is not implemented.")
     lhsT = np.random.normal(size=(K, M)).astype(data_type)
     rhs = np.random.normal(size=(K, N)).astype(data_type)
-    jobs = ProfileJobs()
-    for config_id, config in enumerate(configs):
-        # kernel_code = specialize_kernel(lhsT_rhs_gemm_general, ["maybe_init", "maybe_compute", "maybe_save"], **config)
-        # generated_file = f"generated_kernels/generated_lhsT_rhs_{config_id}.py"
-        # save_code_to_file(generated_file, kernel_code, lhsT_rhs_gemm_general)
+    configs = get_configs()
+    for config in configs:
         jobs.add_job(
-            kernel=("autotune/modules/meta_lhsT_rhs.py", "lhsT_rhs_gemm_general"),
+            kernel=("autotune/modules/lhsT_rhs.py", "lhsT_rhs_gemm"),
             input_tensors=(lhsT, rhs),
             kernel_kwargs=config,
             compiler_flags="--target=trn1 --auto-cast=none --internal-tensorizer-opt-level=nki",
-            preprocessing=None,
+            preprocessing=GEMMCompatibility(transposed_lhs=True),
             postprocessing=postprocessing,
         )
-    jobs.add_job(
-        kernel=("autotune/modules/lhsT_rhs.py", "lhsT_rhs_gemm"),
-        input_tensors=(lhsT, rhs),
-        kernel_kwargs={"NUM_BLOCK_M": 2, "NUM_BLOCK_N": 1, "NUM_BLOCK_K": 4, "template": "MNK"},
-        compiler_flags="--target=trn1 --auto-cast=none --internal-tensorizer-opt-level=nki",
-        preprocessing=None,
-        postprocessing=postprocessing,
-    )
-    jobs.add_job(
-        kernel=("autotune/modules/lhsT_rhs.py", "lhsT_rhs_gemm"),
-        input_tensors=(lhsT, rhs),
-        kernel_kwargs={"NUM_BLOCK_M": 2, "NUM_BLOCK_N": 1, "NUM_BLOCK_K": 4, "template": "KMN"},
-        compiler_flags="--target=trn1 --auto-cast=none --internal-tensorizer-opt-level=nki",
-        preprocessing=None,
-        postprocessing=postprocessing,
-    )
     return jobs
 
 
 if __name__ == "__main__":
-    cache_root_dir = "/home/ubuntu/autotune-cache"
-    # configs = get_configs()
-    # jobs = get_jobs(M=512, N=512, K=10240, configs=configs)
-    # tuner = Benchmark(jobs=jobs, cache_root_dir=cache_root_dir)
-    # tuner()
-    # kernels = ["lhsT_rhs_gemm_general"]
-    # plot_metric(cache_root_dir, "min_ms", kernels)
-    # plot_metric(cache_root_dir, "mfu_estimated_percent", kernels)
-
-    M = 512
-    N = 512
-    K = 10240
-    data_type = "float32"
-    lhsT = np.random.normal(size=(K, M)).astype(data_type)
-    rhs = np.random.normal(size=(K, N)).astype(data_type)
-    config = {
-        "NUM_BLOCK_M": 2,
-        "NUM_BLOCK_N": 1,
-        "NUM_BLOCK_K": 4,
-        "loop_order": "MNK",
-        "tensor_positions": {"lhsT_block": 0, "rhs_block": 1},
-    }
-    kernel = MetaTGEMM(**config)
-    kernel(lhsT, rhs)
+    cache_root_dir = "/mnt/efs/autotune-cache"
+    mn_shapes = [4096]
+    k_shapes = [8192]
+    MNK = list(product(mn_shapes, mn_shapes, k_shapes))
+    jobs = ProfileJobs()
+    for M, N, K in MNK:
+        add_jobs(jobs, M, N, K)
+    tuner = Benchmark(jobs=jobs, cache_root_dir=cache_root_dir)
+    tuner()
+    kernels = ["lhsT_rhs_gemm"]
+    plot_metric(cache_root_dir, "min_ms", kernels)
+    plot_metric(cache_root_dir, "mfu_estimated_percent", kernels)
