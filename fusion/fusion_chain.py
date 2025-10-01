067e202052ccfe8d158c0ad7b1c5c033a9fe5e7b
Wei Tang <weittang@amazon.com>
2025-09-30 23:13:07 -0400

    add fx

--- fusion/fusion_chain.py
+++ fusion/fusion_chain.py
@@ -1,10 +1,10 @@
 import math
-from typing import Dict, Tuple
+from typing import Dict, List, Tuple
 
 import numpy as np
 
 from autotune.core.metrics import check_correctness
-from fusion.operators import Operator, SumSquaresFx
+from fusion.operators import Operator, RMSNormGb, SumSquaresFx
 from fusion.tensors import Tensor
 
 
@@ -29,19 +29,26 @@ class FusionChain:
             raise NotImplementedError(f"Different fusion sizes is not supported.")
         self.fusion_size = fusion_sizes[0]
         self.fx = fx
-        self.gbs = []
-        self.hbs = []
+        self.gbs: List[Operator] = []
+        self.hbs: List[Operator] = []
 
-    def add_operator(self, gb, hb):
+    def add_operator(self, gb: Operator, hb: Operator):
         self.gbs.append(gb)
         self.hbs.append(hb)
 
     def run(self, block_size: int):
         num_fusion_steps = math.ceil(self.fusion_size / block_size)
-        fx_inputs_tensors = tuple([self.input_tensors[tensor_name] for tensor_name in self.fx.input_tensors])
-        prev_x = self.fx.initialize_result(fx_inputs_tensors)
+        fx_input_tensors = self.fx.get_input_tensors(self.input_tensors)
+        prev_x = self.fx.get_initial_value(fx_input_tensors)
         for fusion_step in range(num_fusion_steps):
-            self.fx.step(fusion_step, block_size, fx_inputs_tensors, prev_x)
+            fusion_axis_start = fusion_step * block_size
+            tenor_slices = ()
+            for tensor in fx_input_tensors:
+                tensor_slice = tensor.get_fusion_slice(start=fusion_axis_start, size=block_size)
+                tenor_slices += (tensor_slice,)
+            self.fx.step((prev_x,) + tenor_slices)
+            for gb, hb in zip(self.gbs, self.hbs):
+                gb.step((prev_x,))
         return prev_x.data
 
 
@@ -54,7 +61,8 @@ if __name__ == "__main__":
     rhs = Tensor(name="rhs", axes=("K", "N"), fusion_axis="K", data=np.random.normal(size=(K, N)).astype(data_type))
 
     chain = FusionChain(name="RMSNorm+Matmul", input_tensors=(lhs, rhs), fx=SumSquaresFx("lhs"))
-    chain_result = chain.run(block_size=128)
+    chain.add_operator(gb=RMSNormGb("lhs"), hb=None)
+    chain_result = chain.run(block_size=512)
 
     golden = np.sum(np.square(lhs.data), axis=-1)
     check_correctness(desired=golden, actual=chain_result, atol=1e-5, rtol=1e-5)
