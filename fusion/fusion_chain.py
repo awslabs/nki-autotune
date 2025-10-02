1783631aa0060d57c7cf45669de0c4fd7ff42fba
Wei Tang <weittang@amazon.com>
2025-10-02 13:18:33 -0400

    add FxOperator class

--- fusion/fusion_chain.py
+++ fusion/fusion_chain.py
@@ -1,9 +1,9 @@
 """FusionChain implementation for universal kernel fusion based on the MegaFuse paper."""
 
 import math
-from typing import Dict, List
+from typing import List
 
-from fusion.operators import Operator
+from fusion.operators import FxOperator, Operator
 from fusion.tensors import Tensor
 
 
@@ -15,13 +15,17 @@ class FusionChain:
     enabling fusion of complex patterns.
     """
 
-    def __init__(self, fx: Operator, gbs: List[Operator], hbs: List[Operator], **kwargs):
+    def __init__(self, fx: FxOperator, gbs: List[Operator], hbs: List[Operator]):
         """
         Initialize FusionChain with a sequence of operators.
         """
         self.fx = fx
         self.gbs = gbs
         self.hbs = hbs
+
+    def get_tensors(self, tensor_names: List[str]) -> List[Tensor]:
+        tensors = [self.all_tensors[tensor_name] for tensor_name in tensor_names]
+        return tensors
 
     def execute(self, fusion_axis: str, fusion_block_size: int, input_tensors: List[Tensor]) -> Tensor:
         """
@@ -43,20 +47,19 @@ class FusionChain:
                 else:
                     assert fusion_size == tensor_fusion_size, f"Fusion size mismatch in input tensors"
         assert fusion_size > 0, "Did not find fusion axis in the input tensors"
-        self.all_tensors: Dict[str, Tensor] = {}
-        for tensor in input_tensors:
-            self.all_tensors[tensor.name] = tensor
         num_fusion_steps = math.ceil(fusion_size / fusion_block_size)
-        fx_input_tensors = [self.all_tensors[tensor_name] for tensor_name in self.fx.input_tensors]
+        self.all_tensors = {tensor.name: tensor for tensor in input_tensors}
+
+        fx_input_tensors = self.get_tensors(self.fx.input_tensors)
         prev_O1 = self.fx.initialize_output(fusion_axis, fx_input_tensors)
-        curr_O1 = Tensor(name=prev_O1.name.replace("prev", "curr"), axes=prev_O1.axes, data=prev_O1.data)
+        curr_O1 = Tensor(name="curr_O1", axes=prev_O1.axes, data=prev_O1.data)
         for fusion_step in range(num_fusion_steps):
             fusion_axis_start = fusion_step * fusion_block_size
-            fx_forward_inputs = [prev_O1]
+            fx_forward_inputs = []
             for tensor in fx_input_tensors:
                 fx_forward_inputs.append(
                     tensor.get_axis_slice(fusion_axis, start=fusion_axis_start, size=fusion_block_size)
                 )
-            self.fx.forward(inputs=fx_forward_inputs, next_output=curr_O1)
+            self.fx.forward(curr_O1=curr_O1, prev_O1=prev_O1, input_tensors=fx_forward_inputs)
             prev_O1.data = curr_O1.data
         return curr_O1
--- fusion/operators.py
+++ fusion/operators.py
@@ -23,3 +23,25 @@ class Operator(ABC):
     @abstractmethod
     def initialize_output(self, fusion_axis: str, inputs: List[Tensor]) -> Tensor:
         """Initialize the output tensor given inputs."""
+
+
+class FxOperator(ABC):
+    def __init__(self, input_tensors: List[str]) -> None:
+        super().__init__()
+        self.input_tensors = input_tensors
+
+    @abstractmethod
+    def forward(self, curr_O1: Tensor, prev_O1: Tensor, input_tensors: List[Tensor]) -> None:
+        """
+        Execute the operator forward pass.
+        Update curr_O1 in place.
+
+        Args:
+            curr_O1 (Tensor): O1_k
+            prev_O1 (Tensor): O1_k-1
+            input_tensors (List[Tensor]): V1_k
+        """
+
+    @abstractmethod
+    def initialize_output(self, fusion_axis: str, input_tensors: List[Tensor]) -> Tensor:
+        """Initialize the output tensor given input_tensors."""
