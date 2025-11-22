f2060cb24db289e1a26d0640008674c9d54a1903
weiT1993 <weittang@amazon.com>
2025-11-22 05:15:36 +0000

    Separate HBM and buffer tensor types

--- compute_graph/operators.py
+++ compute_graph/operators.py
@@ -1,8 +1,52 @@
-from compute_graph.nodes import Node
-from compute_graph.tensors import Axis
+class BufferNode:
+    """Base class for on-chip operators."""
+
+    def __init__(self, op_code: str, inputs: list[str], outputs: list[str], tensor_axes: dict[str, list[str]]) -> None:
+        """
+        Args:
+            op_code: Operation code identifier
+            inputs: List of input tensor names
+            outputs: List of output tensor names
+            tensor_axes: Mapping of tensor names to their axis names
+        """
+        self.op_code = op_code
+        self.inputs = inputs
+        self.outputs = outputs
+        self.tensor_axes = tensor_axes
+        self.axis_sizes: dict[str, dict[str, int]] = {}
+
+    @property
+    def is_specialized(self) -> bool:
+        specialized = True
+        for tensor in self.inputs:
+            if tensor in self.axis_sizes:
+                tensor_axes = self.tensor_axes[tensor]
+                for axis in tensor_axes:
+                    if axis not in self.axis_sizes[tensor]:
+                        specialized = False
+                        break
+            else:
+                specialized = False
+                break
+        return specialized
+
+    def specialize(self, tensor_name: str, axis: str, size: int) -> None:
+        if tensor_name not in self.axis_sizes:
+            self.axis_sizes[tensor_name] = {}
+        self.axis_sizes[tensor_name][axis] = size
+
+    def get_tensor_shape(self, tensor_name: str) -> tuple[int, ...]:
+        raise NotImplementedError(f"get_tensor_shape is not implemented for {self}")
+
+    def codegen(self) -> str:
+        """Generate NKI code for this node."""
+        raise NotImplementedError(f"codegen is not implemented for {self}")
+
+    def clear_specialization(self) -> None:
+        self.axis_sizes.clear()
 
 
-class TensorScalar(Node):
+class TensorScalar(BufferNode):
     """
     Tensor-scalar operator.
     dest[...] = nisa.tensor_scalar(data, op0, operand0, op1, operand1)
@@ -38,7 +82,7 @@ class TensorScalar(Node):
         return [self.dest]
 
 
-class Activation(Node):
+class Activation(BufferNode):
     """
     Activation operator with optional reduce.
     With reduce:
@@ -77,7 +121,7 @@ class Activation(Node):
         return write_names
 
 
-class Transpose(Node):
+class Transpose(BufferNode):
     """Transpose operator."""
 
     def __init__(self, dest: str, **kwargs) -> None:
@@ -98,7 +142,7 @@ class Transpose(Node):
         return [self.dest]
 
 
-class Matmul(Node):
+class Matmul(BufferNode):
     """Matrix multiplication operator."""
 
     def __init__(self, dest: str, **kwargs) -> None:
@@ -138,7 +182,7 @@ class Matmul(Node):
         return [self.dest]
 
 
-class Allocate(Node):
+class Allocate(BufferNode):
     """
     Allocate operator.
     """
@@ -149,46 +193,6 @@ class Allocate(Node):
     @property
     def read_tensor_names(self) -> list[str]:
         return []
-
-    @property
-    def write_tensor_names(self) -> list[str]:
-        return [self.dest]
-
-
-class Load(Node):
-    """
-    Load operator.
-    """
-
-    def __init__(self, dest: str, src: str, axes: tuple[Axis, ...]) -> None:
-        super().__init__(op_code="nl.load", dest=dest)
-        self.src = src
-        self.axes = axes
-
-    @property
-    def read_tensor_names(self) -> list[str]:
-        return [self.src]
-
-    @property
-    def write_tensor_names(self) -> list[str]:
-        return [self.dest]
-
-    def __repr__(self) -> str:
-        code = f"{self.dest} = nl.load(src={self.src}{list(self.axes)})"
-        return code
-
-
-class Store(Node):
-    """
-    Store operator.
-    """
-
-    def __init__(self, dest: str, **kwargs) -> None:
-        super().__init__(op_code="nl.store", dest=dest, **kwargs)
-
-    @property
-    def read_tensor_names(self) -> list[str]:
-        return [self.kwargs["value"]]
 
     @property
     def write_tensor_names(self) -> list[str]:
